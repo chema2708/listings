7bcada0eaef01abb7c21f6d0603cc5fd96d13a93
raul silva <raul.silva@pslgroup.com>
2019-02-27 15:50:24 -0600

    raul-listing: changes added with styles for card; specialties added from the component to the view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,15 +28,20 @@ export class AppComponent implements OnInit {
     })
   };
   programs: Array<any> = [];
-
+  main_programs: Array<any> = [];
   constructor (private http: HttpClient) { }
 
   ngOnInit() {
     this.getList().subscribe(data => {
       if (data) {
         for (let d in data["hits"]["hits"]) {
-          this.programs.push(data["hits"].hits[d]);
+          if (d == 0 ) {
+            this.main_programs.push(data["hits"].hits[d]);
+          } else {
+            this.programs.push(data["hits"].hits[d]);
+          }
         }
+          this.specialties =  ['Oncology','Cardiology','Internal Medicine','Neurology','Paediatrics'];
       }
     });
   }
